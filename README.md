89d6c98b2d283271023970f519765c1da4694e6a
Jan Scheiper <janscheiper95@gmail.com>
2019-12-08 14:17:36 +0100

    version 1.1.0: add config option to disable compression

--- README.md
+++ README.md
@@ -12,12 +12,14 @@ Install the plugin globally by running
 
 The plugin only needs a single configuration key, which is called `imgUrl`.
 Usually, this you be some like `http://localhost:8081/current`, to access the current frame of your Motion live server running on localhost.
-While this is the intended use, you could also plug in any old URL pointing to a JPEG image.
+While this is the intended use, you could also plug in any old URL pointing to a JPEG image, for example `http://localhost:8080/?action=snapshot` if you're using MJPEG-streamer.
+If you get an error like _Corrupt JPEG data: 2 extraneous bytes before marker 0xd6_, you might have to set `compression` to `false` (default: **true**), since the JPEG image is malformed in some way.
 
 Example:
 ```json
 {
-    "imgUrl": "http://localhost:8081/current"
+    "imgUrl": "http://localhost:8081/current",
+    "compression": true
 }
 ```
 
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class MotionWebcamPlugin extends Plugin {
 
 	handleFetch(payload, reply) {
 		let imgUrl = this.config.imgUrl;
+		let compression = ("compression" in this.config) ? this.config.compression : true;
 
 		if (imgUrl == undefined) {
 			return reply({ error: "No imgUrl specified" });
@@ -21,15 +22,19 @@ class MotionWebcamPlugin extends Plugin {
 		}, (err, resp, body) => {
 			if (err) return reply({ error: err.message });
 			
-			imagemin.buffer(body, {
-				plugins: [
-					imageminJpegtran()
-				]
-			}).then(img => {
-				reply({ image: img.toString("base64") });
-			}).catch(err => {
-				reply({ error: err.message });
-			});
+			if (compression) {
+				imagemin.buffer(body, {
+					plugins: [
+						imageminJpegtran()
+					]
+				}).then(img => {
+					reply({ image: img.toString("base64") });
+				}).catch(err => {
+					reply({ error: err.message });
+				});
+			} else {
+				reply({ image: body.toString("base64") });
+			}
 		});
 	}
 }
